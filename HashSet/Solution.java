b9b40b2e2b5f77bd78273fecb8fb4df4b23e52e5
Ahmad madani <163474796+Ahmadmadani265@users.noreply.github.com>
2025-08-22 22:46:31 +0300

    data

---
+++ HashSet/Solution.java
@@ -0,0 +1,105 @@
+package HashSet;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Solution {
+    //Q1:
+    static void miss(int[] arr, int target) {
+       HashSet<Integer>a=new HashSet();
+       ArrayList<Integer>s=new ArrayList();
+       for(int x:arr){
+           a.add(x);
+       }
+       for(int i=1;i<=target;i++){
+           if(!a.contains(i)){
+               s.add(i);
+           }
+       }
+        System.out.println(s);
+    }
+    
+    //Q2:
+    static void pair(int[] arr, int target) {
+      HashSet<Integer>s=new HashSet();
+      Set<List<Integer>>res=new HashSet();
+      for(int x:arr){
+          int com=target-x;
+          if(s.contains(com)){
+              List<Integer>pair=Arrays.asList(x,com);
+              res.add(pair);
+          }
+          s.add(x);
+      }
+        System.out.println(res);
+    }
+    //Q3:
+     static void comm(int[] arr, int[] arr1) {
+        HashSet<Integer>a=new HashSet();
+        for(int x:arr){
+            a.add(x);
+        }
+        int c=0;
+        for(int x:arr1){
+            if(a.contains(x)){
+                c++;
+            }
+        }
+          System.out.println(c);
+    }
+     //Q4:
+     static void uncomm(String s1, String s2) {
+        HashSet<Character>a=new HashSet();
+        HashSet<Character>b=new HashSet();
+        ArrayList<Character>ch=new ArrayList();
+         for (char x:s1.toCharArray()) {
+             a.add(x);
+         }
+         for (char x:s2.toCharArray()) {
+             b.add(x);
+         }
+         for(char x:s2.toCharArray()){
+             if(!a.contains(x)){
+                 ch.add(x);
+             }
+         }
+           for(char x:s1.toCharArray()){
+             if(!b.contains(x)){
+                 ch.add(x);
+             }
+         }
+         System.out.println(ch);
+         
+    }
+     //Q5:
+      static void removedup(String s) {
+           HashSet<Character>a=new HashSet();
+           String s1="";
+           for(char ch:s.toCharArray()){
+               if(!a.contains(ch)){
+                   a.add(ch);
+                   s1+=ch;
+               }
+               else{
+                   a.clear();
+                   
+               }
+           }
+           System.out.println(s1);
+        
+    }
+
+
+    public static void main(String[] args) {
+        int arr[]={1,2,7,8,9};
+        int arr1[]={1,2,3,5,6};
+        pair(arr,6);
+        miss(arr,6);
+        comm(arr,arr1);
+        uncomm("abcd","afgh");
+        removedup("aabccbaa");
+    } 
+}
---
+++ MapSet/Solution.java
@@ -0,0 +1,97 @@
+package MapSet;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Solution {
+      //  Q1:
+    static void frequency(){
+        Scanner in=new Scanner(System.in);
+        System.out.print("Enter word=");
+        String s=in.nextLine();
+        String word[]=s.split(" ");
+        Map<String,Integer>m=new HashMap();
+        for (int i = 0; i <word.length; i++) {
+            m.put(word[i],m.getOrDefault(word[i],0)+1);
+        }
+        System.out.println(m);
+    }
+    //Q2:
+    static void removeall_duplicate(){
+        Scanner in=new Scanner(System.in);
+        System.out.print("Enter word=");
+        String s=in.next();
+        String s1="";
+        Map<Character,Boolean>m=new HashMap();
+        for(char ch:s.toCharArray()){
+            if(!m.containsKey(ch)){
+                m.put(ch,true);
+                s1+=ch;
+            }
+        }
+        System.out.println(s1);
+    }
+   // Q3:
+    static void merge(Map<Integer,Integer>m,Map<Integer,Integer>m1){
+        for(Integer x:m1.keySet()){
+            if(m.containsKey(x)){
+                m.put(x,m.get(x)+m1.get(x));
+            }
+            else{
+                m.put(x,m1.get(x));
+            }
+        }
+        System.out.println(m);
+    }
+    //Q4:
+    static void longest_Substring(){
+        Scanner in=new Scanner(System.in);
+        System.out.print("Enter word=");
+        String s=in.next();
+         Map<Character,Integer>m=new HashMap();
+         int start=0,max=0;
+         for(int i=0;i<s.length();i++){
+             char ch=s.charAt(i);
+             if(m.containsKey(ch)&&m.get(ch)>=start){
+                 start=m.get(ch)+1;
+             }
+             m.put(ch, i);
+             max=Math.max(max,i-start+1);
+         }
+         System.out.println(max);
+    }
+     static void frequency_character(){
+        Scanner in=new Scanner(System.in);
+        System.out.print("Enter word=");
+        String s=in.next();
+         Map<Character,Integer>m=new HashMap();
+          for (char ch:s.toCharArray()) {
+            m.put(ch,m.getOrDefault(ch,0)+1);
+        }
+           Map<Integer,String>m1=new HashMap();
+           for(Map.Entry x:m.entrySet()){
+               m1.put((Integer) x.getValue(),m1.getOrDefault(x.getValue(),"")+x.getKey());
+           }
+           System.out.println(m1);
+          
+    }
+    
+    public static void main(String[] args) {
+       Map<Integer,Integer>m=new HashMap();
+       m.put(1,5);
+       m.put(2,6);
+       m.put(3,7);
+       m.put(4,8);
+       Map<Integer,Integer>m1=new HashMap();
+       m1.put(1,5);
+       m1.put(7,6);
+       m1.put(3,2);
+       m1.put(5,8);
+       frequency();
+       removeall_duplicate();
+      merge(m,m1);
+      longest_Substring();
+      frequency_character();
+    }
+}
