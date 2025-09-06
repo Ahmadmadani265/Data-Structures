fa6042a1b7d5aef52dae0d979499c63884f41f25
Ahmad Madani <163474796+Ahmadmadani265@users.noreply.github.com>
2025-09-06 19:29:40 +0300

    Update Solutions.java

--- Questions/Stack/Solutions.java
+++ Questions/Stack/Solutions.java
@@ -49,5 +49,49 @@ public class Solutions {
             s.push(x);
         }
     }
+   // Q4:
+   public class StackUsingQueues {
+    Queue<Integer> q1 = new LinkedList<>();
+    Queue<Integer> q2 = new LinkedList<>();
+
+    void push(int x) {
+        q2.add(x);
+        while (!q1.isEmpty()) {
+            q2.add(q1.remove());
+        }
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+    }
+
+    int pop() {
+        if (q1.isEmpty()) return -1;
+        return q1.remove();
+    }
+
+    int top() {
+        if (q1.isEmpty()) return -1;
+        return q1.peek();
+    }
+
+    boolean isEmpty() {
+        return q1.isEmpty();
+    }
     
+    }
+   
+   //Q5:
+   public static Stack<Integer> sortStack(Stack<Integer> st) {
+        Stack<Integer> temp = new Stack<>();
+
+        while (!st.isEmpty()) {
+            int val = st.pop();
+
+            while (!temp.isEmpty() && temp.peek() > val) {
+                st.push(temp.pop());
+            }
+            temp.push(val);
+        }
+        return temp;
+    }
 }
